a79ed0bf3fe6963d88f1870608648fa6056a2304
jdosk <jdosk314@mail.endicott.edu>
2019-04-02 20:22:31 -0400

    Added stats file

---
+++ StormTheCastle/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+
+    public int maxHealth = 100;
+    public int currentHealth { get; private set;  }
+    
+    public Stat stamina;
+    public Stat damage;
+    public bool foundChest;
+    
+}
---
+++ StormTheCastle/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Stat{
+
+    [SerializeField]
+    private int baseValue;
+
+    public int GetStat()
+    {
+        return baseValue;
+    }
+	
+}
