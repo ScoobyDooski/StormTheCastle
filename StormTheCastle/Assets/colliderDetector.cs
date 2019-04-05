5f74fe7812fd058344fee683b19841d421f49c26
Jeremy <jdosk314@mail.endicott.edu>
2019-04-05 01:29:03 -0400

    Added weapons to players hand and collision detection on sword

---
+++ StormTheCastle/Assets/colliderDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderDetector : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Entered");
+    }
+    private void OnCollisionStay(Collision col)
+    {
+        Debug.Log("Stay");
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        Debug.Log("Exit");
+    }
+}
