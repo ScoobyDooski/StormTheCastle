68aa0e616e75b2977c42835161395cb095feb07c
jdosk <jdosk314@mail.endicott.edu>
2019-04-04 23:35:59 -0400

    added enemy animation

---
+++ StormTheCastle/Assets/chest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //pick a spawn location
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("TOUCHING!!!");
+    }
+}
